b790e8a30069ccadb707e1991bbbd8f882d3546f
Kacper Kowalski <kkowalski0477@gmail.com>
2024-03-01 20:05:45 +0100

    Change values program
    An updated version using Helpy class with a new feature of creating your own table of integers filling it with the desired numbers and also displaying it onto the screen

--- change_values.cpp
+++ change_values.cpp
@@ -1,75 +1,114 @@
-﻿#include<iostream>
+#include <iostream>
+#include "Helpy.h"
+using std::cout;
+using std::cin;
 
 
 void Swap(int *x, int *y) {
 
-    std::cout << "number1 = number1 + number2;\n";
-    std::cout << "number2 = number1 - number2\n";
-    std::cout << "number1 = number1 - number2\n\n\n";
+    cout << "number1 = number1 + number2;\n";
+    cout << "number2 = number1 - number2\n";
+    cout << "number1 = number1 - number2\n\n\n";
 
-    std::cout << "In the first line adds both of the numbers together and stores the result as the first number\n\n";
-    *x += *y;   
+    cout << "In the first line adds both of the numbers together and stores the result as the first number\n\n";
+    *x += *y;
 
-    std::cout << "The result:\n";
-    std::cout << "number1 = " << *x << " and number2 = " << *y << '\n';
+    cout << "The result:\n";
+    cout << "number1 = " << *x << " and number2 = " << *y << '\n';
 
-    std::cout << "The second line subtracts your previous result from number2 and stores the result in number2(that's the first swap)\n\n";
+    cout << "The second line subtracts your previous result from number2 and stores the result in number2(that's the first swap)\n\n";
     *y = *x - *y;
 
-    std::cout << "The result:\n";
-    std::cout << "number 1 = " << *x << " and number2 = " << *y << '\n';
+    cout << "The result:\n";
+    cout << "number 1 = " << *x << " and number2 = " << *y << '\n';
 
-    std::cout << "The third line subtracts number1 from number2 and stores the result in number1(that's the second swap)\n\n";
+    cout << "The third line subtracts number1 from number2 and stores the result in number1(that's the second swap)\n\n";
     *x -= *y;
 
-    std::cout << "FINAL RESULT! Here are your numbers after the swap:\n";
-    std::cout << "number1 = " << *x << " and number2 = " << *y << '\n\n';
+    cout << "FINAL RESULT! Here are your numbers after the swap:\n";
+    cout << "number1 = " << *x << " and number2 = " << *y << "\n\n";
 
 }
 
 
-void End_program(char* termination) {
+void DisplayTable(int tab[], int size_tab ){
 
-    std::cout << "Do you want to terminate the program?(press t if you want to terminate or p if you want to proceed): ";
-    std::cin >> *termination;
+    for(int i = 0; i < size_tab; i++){
+        cout << tab[i] << ' ';
+    }
 
-    if (*termination == 't' && *termination == 'T')
-        std::cout << "\nGoodbye then! Thank you for your time :)\n";
-        
 }
 
 
+void FillTable(int tab_1[], int tab_2[], int size_tab_1, int size_tab_2){
 
-int main()
-{
+    cout << "\nYOUR FIRST TABLE:\n";
+    for(int i = 0; i < size_tab_1; i++){
+        cout << "\nEnter the " << i + 1<< " number of your table: ";
+        cin >> tab_1[i];
+    }
+
+    cout << "\nGreat! here are the contents of your first table:\n";
+    DisplayTable(tab_1, size_tab_1);
+
+    cout << "\n\nYOUR SECOND TABLE\n";
+    for(int j = 0; j < size_tab_2; j++){
+
+        cout << "\nEnter the " << j + 1<< " number of your table: ";
+        cin >> tab_2[j];
+
+    }
+
+    cout << "\nGreat! here are the contents of your second table:\n";
+    DisplayTable(tab_2, size_tab_2);
+
+}
+
+
+int main() {
+
     int number_1;
     int number_2;
-    char termination = 'k';
-
-    std::cout << "WELCOME TO CHANGE_VALUES PROGRAM!\n\n\n";
-    std::cout << "INFO:\n";
-    std::cout << "In this program you will swap values of two integers of your choosing, by also explaining the whole entire process";
+    int size_tab_1;
+    int size_tab_2;
 
 
-    while (termination != 'p' && termination != 'T') {
-        std::cout << "Enter a value of the first number: ";
-        std::cin >> number_1;
+    cout << "WELCOME TO CHANGE_VALUES PROGRAM!\n\n\n";
+    cout << "INFO:\n";
+    cout << "In this program you will swap determined (by yourself) values of two integers . The program will also explain how the algorithm works and then you will create your own tables of integers fill it with values and you will Swap the values of two tables.\n";
 
 
-        std::cout << "\nEnter a value of the second number: ";
-        std::cin >> number_2;
+    do{
 
+        cout << "Enter a value of the first number: ";
+        cin >> number_1;
 
-        std::cout << "\nHere are your numbers before the swap:\n";
-        std::cout << "number1 = " << number_1 << " and number2 = " << number_2 << '\n';
+        cout << "\nEnter a value of the second number: ";
+        cin >> number_2;
 
+        cout << "\nHere are your numbers before the swap:\n";
+        cout << "number1 = " << number_1 << " and number2 = " << number_2 << '\n';
 
-        std::cout << "\nSWAP!\n";
+        cout << "\nSWAP!\n";
         Swap(&number_1, &number_2);
 
 
-        End_program(&termination);
+        cout << "Enter the size of your first table: ";
+        cin >> size_tab_1;
+
+        cout << "Enter the size of your second table: ";
+        cin >> size_tab_2;
+
+        int *tab_1 =  new int[size_tab_1];
+        int *tab_2 = new int[size_tab_2];
+
+        FillTable(tab_1, tab_2, size_tab_1, size_tab_2);
+
+        delete[] tab_1;
+        delete[] tab_2;
     }
-    
+    while(!Helpy::ShouldProgramEnd());
+
+
     return 0;
 }
