4006ebcc3d973fc084a9108847452260bd3744cf
Kacper Kowalski <kkowalski0477@gmail.com>
2024-03-02 23:45:41 +0100

    Change_values
    An updated version of change_values program the program is now more transparent and clearer, comments where added with better explanations of the algorithm and new functionality which allows the user to dynamically create his/hers own two tables of integers and swap all of the elements between them.

--- change_values.cpp
+++ change_values.cpp
@@ -4,64 +4,77 @@ using std::cout;
 using std::cin;
 
 
-void Swap(int *x, int *y) {
+void Swap(int *x, int *y) { //this function swaps the values of two integers
 
-    cout << "number1 = number1 + number2;\n";
-    cout << "number2 = number1 - number2\n";
-    cout << "number1 = number1 - number2\n\n\n";
-
-    cout << "In the first line adds both of the numbers together and stores the result as the first number\n\n";
     *x += *y;
-
-    cout << "The result:\n";
-    cout << "number1 = " << *x << " and number2 = " << *y << '\n';
-
-    cout << "The second line subtracts your previous result from number2 and stores the result in number2(that's the first swap)\n\n";
     *y = *x - *y;
-
-    cout << "The result:\n";
-    cout << "number 1 = " << *x << " and number2 = " << *y << '\n';
-
-    cout << "The third line subtracts number1 from number2 and stores the result in number1(that's the second swap)\n\n";
     *x -= *y;
-
-    cout << "FINAL RESULT! Here are your numbers after the swap:\n";
-    cout << "number1 = " << *x << " and number2 = " << *y << "\n\n";
 
 }
 
 
-void DisplayTable(int tab[], int size_tab ){
+void DisplayTable(int tab[], int size_tab ){ //this function displays all elements of a table
 
     for(int i = 0; i < size_tab; i++){
         cout << tab[i] << ' ';
+
     }
 
 }
 
 
-void FillTable(int tab_1[], int tab_2[], int size_tab_1, int size_tab_2){
+void FillTable(int tab_1[], int tab_2[], int size_tab_1, int size_tab_2){ //this function allows the user to fill two tables with values
 
-    cout << "\nYOUR FIRST TABLE:\n";
+    cout << "\n\nYOUR FIRST TABLE:\n";
+
     for(int i = 0; i < size_tab_1; i++){
-        cout << "\nEnter the " << i + 1<< " number of your table: ";
+
+        cout << "Enter the " << i + 1<< " number of your  first table: ";
         cin >> tab_1[i];
+
     }
 
-    cout << "\nGreat! here are the contents of your first table:\n";
+    cout << "\nGreat! Here are the contents of your first table!:\n";
     DisplayTable(tab_1, size_tab_1);
 
-    cout << "\n\nYOUR SECOND TABLE\n";
+    cout << "\nYOUR SECOND TABLE\n";
+
     for(int j = 0; j < size_tab_2; j++){
 
-        cout << "\nEnter the " << j + 1<< " number of your table: ";
+        cout << "Enter the " << j + 1<< " number of your second table: ";
         cin >> tab_2[j];
 
     }
 
-    cout << "\nGreat! here are the contents of your second table:\n";
+    cout << "\nGreat! Here are the contents of your second table!:\n";
     DisplayTable(tab_2, size_tab_2);
 
+}
+
+
+void SwapTables(int tab_1[], int tab_2[], int size_t1, int size_t2) { //this function swaps all the elements of two tables
+
+    int size = size_t1;
+
+    if (size_t1 == size_t2){
+        for (int i = 0; i < size; i++) {
+            Swap(&tab_1[i], &tab_2[i]);
+
+        }
+
+        cout << "\n\nFirst table after the swap:\n";
+        DisplayTable(tab_1, size);
+
+        cout << "\nSecond table after the swap\n";
+        DisplayTable(tab_2, size);
+        cout << "\n\n";
+
+    }
+
+    else{
+        cout << "I'm sorry but the swap could not occur since the tables aren't the same size...\n";
+
+    }
 }
 
 
@@ -73,41 +86,60 @@ int main() {
     int size_tab_2;
 
 
-    cout << "WELCOME TO CHANGE_VALUES PROGRAM!\n\n\n";
+    cout << "WELCOME TO CHANGE_VALUES PROGRAM!\n\n";
     cout << "INFO:\n";
-    cout << "In this program you will swap determined (by yourself) values of two integers . The program will also explain how the algorithm works and then you will create your own tables of integers fill it with values and you will Swap the values of two tables.\n";
+    cout << "In this program you will swap the values of two integers of your choosing (you will be the one creating them and assigning them values) . The program will also explain how the algorithm works and then you will create your own two tables of integers fill it with values and you will Swap elements of these tables (for example the first element of your first table becomes the first element of your second table and so on).\n";
 
 
     do{
 
-        cout << "Enter a value of the first number: ";
-        cin >> number_1;
+        cout << "\nEnter a value of the first number: ";
+        cin >> number_1;    //user enters a number
 
         cout << "\nEnter a value of the second number: ";
-        cin >> number_2;
+        cin >> number_2;    //user enters a second number
 
         cout << "\nHere are your numbers before the swap:\n";
-        cout << "number1 = " << number_1 << " and number2 = " << number_2 << '\n';
+        cout << "number1 = " << number_1 << "  |  number2 = " << number_2 << '\n';
 
-        cout << "\nSWAP!\n";
+
+        cout << "\nSWAP!\n\n";
+
+        cout << "EXPLANATION OF SWAP ALGORITHM:\n";
+        cout << "number1 = number1 + number2\n";
+        cout << "1.Sum - here the program creates a sum of number1 and number two and stores the result in number1\n\n";
+        cout << "number2 = number1 - number2\n";
+        cout << "2.First Subtraction - in the next phase of the algorithm the program subtracts number1 (its new sum value) with number2 and stores the result (which is the initial value of number1 before the first step) in number1\n\n";
+        cout << "number1 = number1 - number2\n";
+        cout << "3.Second subtraction - the concept is the same as in the first subtraction it just stores the result (which is the initial value of number2) in number2\n\n";
         Swap(&number_1, &number_2);
 
+        cout << "Here are your numbers after the swap:\n";
+        cout << "number1 = " << number_1 << "  |  number2 = " << number_2 << "\n\n";
 
-        cout << "Enter the size of your first table: ";
-        cin >> size_tab_1;
+        cout << "Okay, now let's move on to tables. In order to create a table you need to determine its size first\n";
+        cout << "NOTE: if you would like to swap the elements of two tables then keep in mind, that they've got to be the same size!\n";
+        cout << "\nEnter the size of your first table: ";
+        cin >> size_tab_1; //user enters a size of the first table
 
         cout << "Enter the size of your second table: ";
-        cin >> size_tab_2;
+        cin >> size_tab_2; //user enters the size of the second table
+
 
         int *tab_1 =  new int[size_tab_1];
-        int *tab_2 = new int[size_tab_2];
+        int *tab_2 = new int[size_tab_2]; //Here two tables are created dynamically
 
-        FillTable(tab_1, tab_2, size_tab_1, size_tab_2);
+        cout << "\nGreat! Now let's fill these tables with values!\n";
+        FillTable(tab_1, tab_2, size_tab_1, size_tab_2); //fills both of these tables with integers
+
+
+        SwapTables(tab_1, tab_2, size_tab_1, size_tab_2 );  //swaps the elements between two tables
+
 
         delete[] tab_1;
-        delete[] tab_2;
+        delete[] tab_2; //deletes the tables
     }
-    while(!Helpy::ShouldProgramEnd());
+    while(!Helpy::ShouldProgramEnd()); //this method is responsible for asking the user whether the program should end or not
 
 
     return 0;
