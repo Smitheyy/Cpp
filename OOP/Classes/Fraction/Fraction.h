537124c59166b51a4c04278acc21e0713f2fbd50
Kacper Kowalski <kkowalski0477@gmail.com>
2024-04-14 02:43:01 +0200

    Organisation

---
+++ OOP/Classes/Fraction/Fraction.h
@@ -0,0 +1,17 @@
+class Fraction {
+private:
+    int n;
+    int d;
+
+public:
+
+    Fraction();
+    Fraction(int nominator, int denominator);
+
+    void Displayfraction();
+    void Findinversefraction();
+    void Modifynominator(int modification);
+    void Modifydenominator(int modification);
+        
+};
+
