b8c73b7ef9e7e17a1b9957d701f5094b8e560bb0
Kacper Kowalski <kkowalski0477@gmail.com>
2024-05-31 19:34:27 +0200

    List of people

---
+++ OOP/virtual_methods, container_classes and polymorphism/Ex.2.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include <utility>
+
+class Person {
+private:
+    std::string name;
+    std::string gender;
+    int age;
+    int height;
+
+public:
+
+    Person() : age(0), height(0) {}
+    Person(std::string _name, std::string _gender, int _age, int _height)
+            : name(std::move(_name)), gender(std::move(_gender)), age(_age), height(_height) {}
+
+    void WriteData(std::ostream& os) const {
+        os << "name: " << name << ' ' << "gender: " << gender << ' '
+           << "age: " << age << ' ' << "height: " << height << '\n';
+        // Check if the stream has failed
+        if (!os.good()) {
+            std::cerr << "Failed to write person data.\n";
+        }
+    }
+};
+
+class SimpleListofPeople {
+private:
+    Person people[2];
+    static int counter_of_people;
+
+public:
+    void Add(const Person& person) {
+        if (counter_of_people < 2) {
+            people[counter_of_people] = person;
+            counter_of_people++;
+        } else {
+            std::cout << "I'm sorry but you can't add more people to the list!\n";
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const SimpleListofPeople& list);
+};
+
+std::ostream& operator<<(std::ostream& os, const SimpleListofPeople& list) {
+    for (int i = 0; i < 2; i++) {
+        os << (i + 1) << '.';
+        list.people[i].WriteData(os);
+    }
+    return os;
+}
+
+int SimpleListofPeople::counter_of_people = 0;
+
+
+int main() {
+    Person jack("Jack", "male", 22, 190), ariana("Ariana", "female", 18, 160);
+    SimpleListofPeople list;
+
+    list.Add(jack);
+    list.Add(ariana);
+
+    std::cout << list;
+
+    return 0;
+}
