090349364582ec4ac183e238b2623e0c37c54f3d
Kacper Kowalski <kkowalski0477@gmail.com>
2024-06-07 10:42:57 +0200

    main
    Main file of Human Student and SimpleListofPeople classes (showcase)

---
+++ OOP/virtual_methods, container_classes and polymorphism/Human_and_SimpleListofPeople3_main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Human.h"
+#include "SimpleListofPeople3.h"
+using namespace std;
+
+
+int main()
+{
+    Human fabian("Fabian"), oliwier("Oliwier");
+    Student* kacper = Student::createNew();
+    Student milena("Milena", 113210);
+
+    SimpleListofPeople3 list(3);
+
+    cout << fabian;
+    cout << oliwier;
+    cout << *kacper;
+
+    cout << "----------------------------------------------------------------------\n";
+
+    list.add(fabian);
+    list.add(oliwier);
+
+    cout << list;
+
+    list.add(*kacper);
+    list.add(milena);
+
+    cout << list;
+
+    
+    delete kacper;
+
+
+    return 0;
+}
