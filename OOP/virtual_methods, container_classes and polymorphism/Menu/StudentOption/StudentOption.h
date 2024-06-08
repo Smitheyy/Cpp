03e86021121e75c7651991c2881e139490a38fc5
Kacper Kowalski <kkowalski0477@gmail.com>
2024-06-08 21:49:47 +0200

    StudentOption class
    One of menu options which allows the user to add a student at the end of list of people

--- OOP/virtual_methods, container_classes and polymorphism/Menu/StudentOption/StudentOption.h
+++ OOP/virtual_methods, container_classes and polymorphism/Menu/StudentOption/StudentOption.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "OptionMenu.h"
 #include "Student2.h"
+#include "ListofPeople2.h"
 #include <iostream>
 
 
@@ -8,7 +9,7 @@ class StudentOption : public OptionMenu
 {
 public:
 	StudentOption() : OptionMenu("Add a new student onto the list\n"){}
-	virtual void Execute(ListofPeople& list) override{
+	virtual void Execute(ListofPeople2& list) override{
 		list.add(Student2::createNew());
 	}
 };
