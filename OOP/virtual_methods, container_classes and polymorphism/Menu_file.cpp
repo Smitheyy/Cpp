aac222f8ad44ef0eca754852d5f5d71fede61215
Kacper Kowalski <kkowalski0477@gmail.com>
2024-06-08 21:15:28 +0200

    main
    Main file of menu class (showcase)

---
+++ OOP/virtual_methods, container_classes and polymorphism/Menu_file.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "ListofPeople.h"
+#include "Menu.h"
+#include "Human2.h"
+using namespace std;
+
+
+int main() {
+
+	ListofPeople list(4);
+
+	Menu menu;
+	char exit_loop = 'q';
+
+	while (exit_loop != 'e') {
+		menu.chooseOption(list);
+		cout << "Do you want to close the menu? (enter e in order to close it): ";
+		cin >> exit_loop;
+	}
+
+	cout << list;
+
+	return 0;
+}
