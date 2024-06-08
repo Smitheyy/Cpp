f9746cd4e69066d078e0967988a35199c7e5d345
Kacper Kowalski <kkowalski0477@gmail.com>
2024-06-08 23:14:30 +0200

    Add files via upload
    update

--- OOP/virtual_methods, container_classes and polymorphism/ListofPeople2/ListofPeople2.h
+++ OOP/virtual_methods, container_classes and polymorphism/ListofPeople2/ListofPeople2.h
@@ -7,7 +7,7 @@ class ListofPeople2
 private:
 	std::vector<Person*> people;
 	int counter_of_people;		// counts how many people were added into the list
-	int number_of_people;	// its value represents the number of people that are supposed to be in the list
+	const int number_of_people;	// its value represents the number of people that are supposed to be in the list
 	int counter_of_students;	// holds info about student's position
 
 public: // interface
