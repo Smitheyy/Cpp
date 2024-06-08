3dda57220c37eccb0a2652f477fbb845177a609a
Kacper Kowalski <kkowalski0477@gmail.com>
2024-06-08 21:46:48 +0200

    HumanOption class
    One of menu options which allows the user to add a human at the end of the list of people

--- OOP/virtual_methods, container_classes and polymorphism/Menu/HumanOption/HumanOption.h
+++ OOP/virtual_methods, container_classes and polymorphism/Menu/HumanOption/HumanOption.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "OptionMenu.h"
 #include "Human2.h"
-#include "ListofPeople.h"
+#include "ListofPeople2.h"
 #include <iostream>
 
 
@@ -9,7 +9,7 @@ class HumanOption : public OptionMenu
 {
 public:
 	HumanOption() : OptionMenu("Add a new person onto the list\n"){}
-	virtual void Execute(ListofPeople& list) override {
+	virtual void Execute(ListofPeople2& list) override {
 		list.add(Human2::createNew());
 	}
 };
